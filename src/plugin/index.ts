97dad6ad9751e73b390b7f53c5679a8a9421b498
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2022-08-17 03:01:49 +0200

    fix

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -3,26 +3,29 @@ import { FormsPlugin } from "./types";
 import AppModal from "./component";
 import { createMoBrixDesignerPlugin } from "mobrix-designer-tools";
 
-const formsPlugin: FormsPlugin = createMoBrixDesignerPlugin("forms", () => ({
-  externalComponent: AppModal,
-  field: (creator) => {
-    let formsConfig = creator.forms || {};
-    let getFormType = formsConfig.getFormType;
-    let getModalVisibility = formsConfig.getModalVisibility;
+const formsPlugin: FormsPlugin = createMoBrixDesignerPlugin(
+  "mobrix-designer-forms",
+  () => ({
+    externalComponent: AppModal,
+    field: (creator) => {
+      let formsConfig = creator.forms || {};
+      let getFormType = formsConfig.getFormType;
+      let getModalVisibility = formsConfig.getModalVisibility;
 
-    let getDarkMode = formsConfig.getDarkMode;
+      let getDarkMode = formsConfig.getDarkMode;
 
-    return {
-      name: "forms",
-      content: {
-        modals: formsConfig.modals,
-        getDarkMode: getDarkMode,
-        getFormType: getFormType,
-        getModalVisibility: getModalVisibility,
-        onClose: formsConfig.onClose,
-      },
-    };
-  },
-}));
+      return {
+        name: "forms",
+        content: {
+          modals: formsConfig.modals,
+          getDarkMode: getDarkMode,
+          getFormType: getFormType,
+          getModalVisibility: getModalVisibility,
+          onClose: formsConfig.onClose,
+        },
+      };
+    },
+  })
+);
 
 export default formsPlugin;
